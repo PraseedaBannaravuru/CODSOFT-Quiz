49a210e23555f678d0132bf6bb9e56cb9033cff2
praseeda <163273190+PraseedaBannaravuru@users.noreply.github.com>
2024-06-24 17:25:23 +0530

    Add files via upload

---
+++ Questions.js
@@ -0,0 +1,59 @@
+const questions = [
+  {
+     number: 1,
+      question: 'Which company developed the JavaScript language?',
+      answer: 'Netscape',
+      options: [
+          'Microsoft',
+          'Apple',
+          'Netscape',
+          'IBM'
+      ]
+  },
+  {
+      number: 2,
+      question: 'What does SQL stand for?',
+      answer: 'Structured Query Language',
+      options: [
+          'Simple Query Language',
+          'Structured Question Language',
+          'Structured Query Language',
+          'System Query Language'
+      ]
+  },
+  {
+      number: 3,
+      question: 'Which programming language is used to build Android apps?',
+      answer: 'Java',
+      options: [
+          'Python',
+          'Java',
+          'Swift',
+          'Kotlin'
+      ]
+  },
+  {
+      number: 4,
+      question: 'Who is the father of the C programming language?',
+      answer: 'Dennis Ritchie',
+      options: [
+          'Brian Kernighan',
+          'Ken Thompson',
+          'Dennis Ritchie',
+          'Linus Torvalds'
+      ]
+  },
+  {
+      number: 15,
+      question: 'Which of the following is a relational database management system?',
+      answer: 'MySQL',
+      options: [
+          'MongoDB',
+          'SQLite',
+          'MySQL',
+          'Redis'
+      ]
+  },
+]
+    
+
